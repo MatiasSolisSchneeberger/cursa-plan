38958da2fbdca89663928098c2741be29b991e4b
MatiasSolisSchneeberger <Matias.Solis.Sch@gmail.com>
2024-11-28 10:01:05 -0300

    argegando dark theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import node from '@astrojs/node';
+
+import icon from 'astro-icon';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), icon(), react()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
