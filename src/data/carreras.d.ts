4305d9b3c2c2ca9980be09c21f10bb275ad72c5a
matias.solis.sch <matias.solis.sch@gmail.com>
2024-12-01 19:16:02 +0000

    terminando encabezado de la materia

--- src/data/carreras.d.ts
+++ src/data/carreras.d.ts
@@ -5,5 +5,26 @@ export interface carreras {
 interface carrera {
     carrera: string;
     emoji: string;
-    color: string;
+    cursada: Array<cursada>;
+}
+
+interface cursada {
+    año: number;
+    cuatri: number;
+    materias: Array<materia>;
+}
+
+interface materia {
+    materia: string;
+    codigo: string;
+    paraCursar: Array<string>;
+    paraRendir: Array<string>;
+    planDeEstudio: string;
+    mesas: Array<mesa>;
+}
+
+interface mesa{
+    dia: number;
+    mes: number;
+    año: number;
 }
